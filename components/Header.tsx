6760182dd099613f9e9d354a6acdb9eb5b1d32c2
bilalzhd <bilalzhd0@gmail.com>
2023-06-26 01:55:49 +0500

    add menu open close

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,139 +1,128 @@
 'use client'
-import Link from "next/link";
-import { useState } from "react";
-import SignIn from "./SignIn";
+import { Fragment, useState } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import SignIn from './SignIn'
+
+const navigation = [
+    { name: 'Home', href: '#', current: true },
+    { name: 'Pricing', href: '#pricing', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function Header() {
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
-    function toggleModal() {
-        setIsLoginModalOpen(!isLoginModalOpen)
-    }
+
     return (
-        <header className="relative z-50 overflow-hidden w-full h-24">
-            <div className="container flex items-center justify-center h-full max-w-6xl px-8 mx-auto sm:justify-between xl:px-0">
-                <Link
-                    href="/"
-                    className="relative flex items-center h-full font-black leading-none"
-                >
-                    <svg
-                        className="w-auto h-6 text-indigo-600 fill-current"
-                        viewBox="0 0 194 116"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <g fillRule="evenodd">
-                            <path d="M96.869 0L30 116h104l-9.88-17.134H59.64l47.109-81.736zM0 116h19.831L77 17.135 67.088 0z" />
-                            <path d="M87 68.732l9.926 17.143 29.893-51.59L174.15 116H194L126.817 0z" />
-                        </g>
-                    </svg>
-                    <span className="ml-3 text-xl text-gray-800">
-                        Landmark<span className="text-pink-500">.</span>
-                    </span>
-                </Link>
-
-                <nav id="nav" className="absolute top-0 left-0 z-40 flex flex-col items-center justify-between w-full h-64 pt-5 mt-24 text-sm text-gray-800 bg-white border-t border-gray-200 md:w-auto md:flex-row md:h-24 lg:text-base md:bg-transparent md:mt-0 md:border-none md:py-0 md:flex md:relative" >
-                    <a href="#" className="ml-0 mr-0 font-bold duration-100 md:ml-12 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600"> Home </a>
-                    <a href="#pricing" className="mr-0 font-bold duration-100 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600">
-                        Pricing
-                    </a>
-                    <div className="flex flex-col w-full font-medium border-t border-gray-200 md:hidden">
-                        <button onClick={toggleModal} className="w-full py-2 font-bold text-center text-pink-500">
-                            Login
-                        </button>
-                        <Link href="/register" className="relative inline-block w-full px-5 py-3 text-sm leading-none text-center text-white bg-indigo-700 fold-bold"> Get Started </Link>
-                    </div>
-                </nav>
-
-                <div className="absolute left-0 flex-col items-center justify-center hidden w-full pb-8 mt-48 border-b border-gray-200 md:relative md:w-auto md:bg-transparent md:border-none md:mt-0 md:flex-row md:p-0 md:items-end md:flex md:justify-between">
-                    <button onClick={toggleModal} className="relative z-40 px-3 py-2 mr-0 text-sm font-bold text-pink-500 md:px-5 lg:text-white sm:mr-3 md:mt-0"> Login </button>
-                    <a
-                        href="#_"
-                        className="relative z-40 inline-block w-auto h-full px-5 py-3 text-sm font-bold leading-none text-white transition duration-300 bg-indigo-700 rounded shadow-md fold-bold lg:bg-white lg:text-indigo-700 sm:w-full lg:shadow-none hover:shadow-xl" >
-                        Get Started
-                    </a>
-                    <svg className="absolute top-0 left-0 hidden w-screen max-w-3xl -mt-64 -ml-12 lg:block" viewBox="0 0 818 815" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                        <defs>
-                            <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="c">
-                                <stop stopColor="#E614F2" offset="0%" />
-                                <stop stopColor="#FC3832" offset="100%" />
-                            </linearGradient>
-                            <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="f">
-                                <stop stopColor="#657DE9" offset="0%" />
-                                <stop stopColor="#1C0FD7" offset="100%" />
-                            </linearGradient>
-                            <filter x="-4.7%" y="-3.3%" width="109.3%" height="109.3%" filterUnits="objectBoundingBox" id="a">
-                                <feOffset dy="8" in="SourceAlpha" result="shadowOffsetOuter1" />
-                                <feGaussianBlur stdDeviation="8" in="shadowOffsetOuter1" result="shadowBlurOuter1" />
-                                <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0" in="shadowBlurOuter1" />
-                            </filter>
-                            <filter
-                                x="-4.7%"
-                                y="-3.3%"
-                                width="109.3%"
-                                height="109.3%"
-                                filterUnits="objectBoundingBox"
-                                id="d">
-                                <feOffset
-                                    dy="8"
-                                    in="SourceAlpha"
-                                    result="shadowOffsetOuter1"
-                                />
-                                <feGaussianBlur
-                                    stdDeviation="8"
-                                    in="shadowOffsetOuter1"
-                                    result="shadowBlurOuter1"
-                                />
-                                <feColorMatrix
-                                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
-                                    in="shadowBlurOuter1"
-                                />
-                            </filter>
-                            <path
-                                d="M160.52 108.243h497.445c17.83 0 24.296 1.856 30.814 5.342 6.519 3.486 11.635 8.602 15.12 15.12 3.487 6.52 5.344 12.985 5.344 30.815v497.445c0 17.83-1.857 24.296-5.343 30.814-3.486 6.519-8.602 11.635-15.12 15.12-6.52 3.487-12.985 5.344-30.815 5.344H160.52c-17.83 0-24.296-1.857-30.814-5.343-6.519-3.486-11.635-8.602-15.12-15.12-3.487-6.52-5.343-12.985-5.343-30.815V159.52c0-17.83 1.856-24.296 5.342-30.814 3.486-6.519 8.602-11.635 15.12-15.12 6.52-3.487 12.985-5.343 30.815-5.343z"
-                                id="b"
-                            />
-                            <path
-                                d="M159.107 107.829H656.55c17.83 0 24.296 1.856 30.815 5.342 6.518 3.487 11.634 8.602 15.12 15.12 3.486 6.52 5.343 12.985 5.343 30.816V656.55c0 17.83-1.857 24.296-5.343 30.815-3.486 6.518-8.602 11.634-15.12 15.12-6.519 3.486-12.985 5.343-30.815 5.343H159.107c-17.83 0-24.297-1.857-30.815-5.343-6.519-3.486-11.634-8.602-15.12-15.12-3.487-6.519-5.343-12.985-5.343-30.815V159.107c0-17.83 1.856-24.297 5.342-30.815 3.487-6.519 8.602-11.634 15.12-15.12 6.52-3.487 12.985-5.343 30.816-5.343z"
-                                id="e"
-                            />
-                        </defs>
-                        <g fill="none" fillRule="evenodd" opacity=".9">
-                            <g transform="rotate(65 416.452 409.167)">
-                                <use fill="#000" filter="url(#a)" xlinkHref="#b" />
-                                <use fill="url(#c)" xlinkHref="#b" />
-                            </g>
-                            <g transform="rotate(29 421.929 414.496)">
-                                <use fill="#000" filter="url(#d)" xlinkHref="#e" />
-                                <use fill="url(#f)" xlinkHref="#e" />
-                            </g>
-                        </g>
-                    </svg>
-                </div>
-
-                <div
-                    id="nav-mobile-btn"
-                    className="absolute top-0 right-0 z-50 block w-6 mt-8 mr-10 cursor-pointer select-none md:hidden sm:mt-10"
-                >
-                    <span className="block w-full h-1 mt-2 duration-200 transform bg-gray-800 rounded-full sm:mt-1"></span>
-                    <span className="block w-full h-1 mt-1 duration-200 transform bg-gray-800 rounded-full"></span>
-                </div>
-            </div>
-            {isLoginModalOpen && (
-                <div id="defaultModal" tabIndex={-1} aria-hidden="true" className="md:top-16 md:left-60 fixed top-0 left-0 z-100 w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full">
-                    <div className="relative w-full max-w-2xl max-h-full">
-                        <div className="relative bg-white rounded-lg shadow">
-                            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                                <h3 className="text-xl font-semibold text-gray-900">
+        <Disclosure as="nav" className="bg-white my-3">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <a className="hidden md:flex relative  items-center h-full font-black leading-none" href="/"><svg className="w-auto h-6 text-indigo-600 fill-current" viewBox="0 0 194 116" xmlns="http://www.w3.org/2000/svg"><g fill-rule="evenodd"><path d="M96.869 0L30 116h104l-9.88-17.134H59.64l47.109-81.736zM0 116h19.831L77 17.135 67.088 0z"></path><path d="M87 68.732l9.926 17.143 29.893-51.59L174.15 116H194L126.817 0z"></path></g></svg><span className="ml-3 text-xl text-gray-800">Landmark<span className="text-pink-500">.</span></span></a>
+                                    {/* <img
+                                        className="hidden h-8 w-auto lg:block"
+                                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                        alt="Your Company"
+                                    /> */}
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    'rounded-md px-3 py-2 text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex space-x-2 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                <button onClick={() => setIsLoginModalOpen(!isLoginModalOpen)} className='text-gray-100 font-bold rounded-lg bg-indigo-500 px-4 py-2'>
                                     Login
-                                </h3>
-                                <button onClick={toggleModal} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-                                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                    <span className="sr-only">Close modal</span>
+                                </button>
+                                <button onClick={() => setIsLoginModalOpen(!isLoginModalOpen)} className='text-gray-800 font-bold rounded-lg bg-gray-200 px-4 py-2'>
+                                    Get started
                                 </button>
                             </div>
-                            <SignIn />
                         </div>
                     </div>
-                </div>)}
-        </header>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block rounded-md px-3 py-2 text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                    {isLoginModalOpen && (
+                        <Transition
+                            as={Fragment}
+                            show={isLoginModalOpen}
+                            enter="transform transition duration-[400ms]"
+                            enterFrom="opacity-0 rotate-[-120deg] scale-50"
+                            enterTo="opacity-100 rotate-0 scale-100"
+                            leave="transform duration-200 transition ease-in-out"
+                            leaveFrom="opacity-100 rotate-0 scale-100 "
+                            leaveTo="opacity-0 scale-95 "
+                        >
+                            <div style={{ zIndex: 100 }} id="defaultModal" tabIndex={-1} aria-hidden="true" className="md:top-16 md:left-60 fixed top-0 left-0 z-100 w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full">
+                                <div className="relative w-full max-w-2xl max-h-full">
+                                    <div className="relative bg-white rounded-lg shadow">
+                                        <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                            <h3 className="text-xl font-semibold text-gray-900">
+                                                Login
+                                            </h3>
+                                            <button onClick={() => setIsLoginModalOpen(!isLoginModalOpen)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                <span className="sr-only">Close modal</span>
+                                            </button>
+                                        </div>
+                                        <SignIn />
+                                    </div>
+                                </div>
+                            </div>
+                        </Transition>)}
+                </>
+            )}
+        </Disclosure>
+
+
     )
 }
+
+
