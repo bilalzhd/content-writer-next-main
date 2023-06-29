2df7154a587fdcab4862ad24d166fb2169a604ab
bilalzhd <bilalzhd0@gmail.com>
2023-06-29 22:53:55 +0500

    sidebar issue fix

--- components/SidebarContainer.tsx
+++ components/SidebarContainer.tsx
@@ -19,8 +19,8 @@ function SidebarContainer() {
                 <Sidebar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
             </div>
             <div className={`fixed top-0 ${isSidebarOpen ? 'hidden' : 'block'}`}>
-                <button className="rounded-circle m-2 flex items-center bg-[rgba(232,234,237,.06)] hover:bg-[rgba(232,234,237,.08)] px-3 py-2 transition-all duration-500 ease-linear">
-                    <Bars3BottomLeftIcon onClick={() => setIsSidebarOpen(!isSidebarOpen)} className="h-8 w-6 text-gray-100 cursor-pointer" />
+                <button onClick={() => setIsSidebarOpen(!isSidebarOpen)} className="rounded-circle m-2 flex items-center bg-[rgba(232,234,237,.06)] hover:bg-[rgba(232,234,237,.08)] px-3 py-2 transition-all duration-500 ease-linear">
+                    <Bars3BottomLeftIcon className="h-8 w-6 text-gray-100 cursor-pointer" />
                 </button>
             </div>
         </>)
